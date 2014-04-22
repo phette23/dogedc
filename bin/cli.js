acdafbc3df5b121d5a73de8d6e7c88eac52a3f06
phette23 <phette23@gmail.com>
2014-04-22 12:20:00 -0400

    cli: pass errors along to process.stderr

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 var dogedc = require('../index'),
-    arg = process.argv[2];
+    arg = process.argv[2],
+    handleError = function (err) {
+        if (err) {
+            process.stderr.write(err.message + '\n');
+            process.exit(1);
+        }
+    };
 
 // arg passed
 if (arg) {
     dogedc(arg, function (err, ddc) {
+        handleError(err);
         process.stdout.write(ddc.dogeClassName + '\n');
     });
 } else {
     // no arg, just do random
     dogedc(function (err, ddc) {
+        handleError(err);
         process.stdout.write(ddc.dogeClassName + '\n');
     });
 }
