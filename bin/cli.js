6a2bea44ba08854d488f3dc47144f2e7a84d04be
phette23 <phette23@gmail.com>
2014-05-10 14:43:02 -0400

    add usage info to CLI

--- bin/cli.js
+++ bin/cli.js
@@ -6,10 +6,19 @@ var dogedc = require('../index'),
             process.stderr.write(err.message + '\n');
             process.exit(1);
         }
+    },
+    usage = function () {
+        console.log('Doge Decimal Classification\n\nUsage:\n\tdogedc [class number]\tPrint Doge Decimal Class. Selects a random class number if none given.');
     };
 
 // arg passed
 if (arg) {
+    // usage info
+    if (['help', '--help', '-h', 'usage'].indexOf(arg) != -1) {
+        usage();
+        process.exit(0);
+    }
+
     dogedc(arg, function (err, ddc) {
         handleError(err);
         process.stdout.write(ddc.dogeClassName + '\n');
